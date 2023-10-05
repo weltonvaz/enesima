23f20908a25ad86363e93d351fffc0f733d515a5
Welton Vaz <weltonvaz@gmail.com>
2023-10-05 10:23:20 -0300

    Segunda versão sobre funções enésimas

--- enesima.py
+++ enesima.py
@@ -1,9 +1,14 @@
-def calcular_raiz(n, num):
-    return num ** (1/n)
+# Função para calcular a raiz enésima de um número inteiro
+def calcular_raiz_enesima(numero, n):
+    if numero < 0 and n % 2 == 0:
+        return "Não é possível calcular a raiz enésima de um número negativo com um expoente par."
+    resultado = numero ** (1/n)
+    return resultado
 
-n = int(input("Digite o valor de n para a raiz enésima: "))
-num = int(input("Digite o número inteiro: "))
+# Solicitar ao usuário o número e o valor de n
+numero = float(input("Digite um número: "))
+n = int(input("Digite o valor de n: "))
 
-resultado = calcular_raiz(n, num)
-
-print(f"A raiz {n}ª de {num} é {resultado}")
+# Calcular e exibir a raiz enésima
+raiz = calcular_raiz_enesima(numero, n)
+print(f"A raiz enésima de {numero} com expoente {n} é igual a {raiz:.2f}")
